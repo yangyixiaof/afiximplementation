2695c6f144fa9695f4ce2185beb476488d8a7200
yyx <yyx@ubuntu>
2016-03-19 08:50:56 -0700

    small change.

--- cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/classmodification/JarModifier.java
+++ cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/classmodification/JarModifier.java
@@ -7,9 +7,13 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.Iterator;
 
+import com.ibm.wala.shrikeBT.Constants;
+import com.ibm.wala.shrikeBT.MethodData;
+import com.ibm.wala.shrikeBT.MethodEditor;
 import com.ibm.wala.shrikeBT.shrikeCT.ClassInstrumenter;
 import com.ibm.wala.shrikeBT.shrikeCT.OfflineInstrumenter;
 import com.ibm.wala.shrikeCT.ClassReader;
+import com.ibm.wala.shrikeCT.ClassWriter;
 import com.ibm.wala.shrikeCT.InvalidClassFileException;
 
 import cn.yyx.labtask.afix.patchgeneration.ExclusivePatchesManager;
@@ -28,22 +32,22 @@ public class JarModifier {
 	private void InitialInstrumentor() throws IllegalArgumentException, IOException {
 		w = new BufferedWriter(new FileWriter("report", false));
 		instrumenter = new OfflineInstrumenter(false);
-		String[] args = new String[]{"TestInputJar/WTest.jar", "-o", "TestOutputJar/WTest.jar"};
+		String[] args = new String[] { "TestInputJar/WTest.jar", "-o", "TestOutputJar/WTest.jar" };
 		instrumenter.parseStandardArgs(args);
 		instrumenter.setPassUnmodifiedClasses(true);
 		instrumenter.addInputClass(new File("selfuseclasscode"),
 				new File("selfuseclasscode/cn/yyx/labtask/afix/LockPool.class"));
 		instrumenter.beginTraversal();
 	}
-	
-	private void DestroyInstrumentor() throws IllegalStateException, IOException
-	{
+
+	private void DestroyInstrumentor() throws IllegalStateException, IOException {
 		instrumenter.close();
 	}
 
-	public void HandleExclusivePatchesManager(ExclusivePatchesManager epm) throws IllegalArgumentException, IOException, InvalidClassFileException {
+	public void HandleExclusivePatchesManager(ExclusivePatchesManager epm)
+			throws IllegalArgumentException, IOException, InvalidClassFileException {
 		InitialInstrumentor();
-		
+
 		int asize = epm.getSize();
 		InitialLockPool(asize);
 		Iterator<SameLockExclusivePatches> itr = epm.Iterator();
@@ -55,25 +59,36 @@ public class JarModifier {
 			// doClass(ci, w);
 			// TODO
 		}
-		
+
 		DestroyInstrumentor();
 	}
 
 	private void InitialLockPool(int asize) throws IOException, InvalidClassFileException {
 		ClassInstrumenter ci = null;
 		while ((ci = instrumenter.nextClass()) != null) {
+			
 			ClassReader cls = ci.getReader();
 			String classname = cls.getName();
-			if (classname.equals("cn/yyx/labtask/afix/LockPool"))
-			{
-				// doClass(ci, w);
-				// TODO
+			if (classname.equals("cn/yyx/labtask/afix/LockPool")) {
+				for (int m = 0; m < ci.getReader().getMethodCount(); m++) {
+					MethodData d = ci.visitMethod(m);
+					
+					MethodEditor me = new MethodEditor(d);
+					me.beginPass();
+					// TODO
+				}
+				ClassWriter cw = ci.emitClass();
+				cw.addField(ClassReader.ACC_PUBLIC | ClassReader.ACC_STATIC, "lock"+idx, "java.util.concurrent.locks.Lock",
+						new ClassWriter.Element[0]);
+				instrumenter.outputModifiedClass(ci, cw);
+				for (int i=0;i<asize;i++)
+				{
+					
+				}
 				
 				break;
 			}
 		}
 	}
-	
-	
-	
+
 }
