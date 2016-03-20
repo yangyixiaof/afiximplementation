3f618fee221fa2f0d947010a794853ea0e49d34a
yyx <yyx@ubuntu>
2016-03-20 00:25:35 -0700

    small change.

--- cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/controlflow/AFixCallGraph.java
+++ cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/controlflow/AFixCallGraph.java
@@ -14,7 +14,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Properties;
 
 import com.ibm.wala.classLoader.IBytecodeMethod;
