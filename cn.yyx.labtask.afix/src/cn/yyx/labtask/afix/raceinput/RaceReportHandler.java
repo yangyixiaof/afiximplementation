daacf235055b8dd81f33bdd91d94f02b6b7ab431
yyx <yyx@ubuntu>
2016-04-09 01:57:53 -0700

    small change.

--- cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/raceinput/RaceReportHandler.java
+++ cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/raceinput/RaceReportHandler.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import cn.yyx.labtask.rvparse.RVStructureVisitor;
+
 public class RaceReportHandler {
 	
 	public static PCRPool ReadReport(File f) throws IOException
@@ -14,7 +16,18 @@ public class RaceReportHandler {
 		String online = null;
 		while ((online = br.readLine()) != null)
 		{
-			
+			if (!online.startsWith("Race:"))
+			{
+				continue;
+			}
+			RVStructureVisitor evalVisitor = new RVStructureVisitor(pp);
+			try {
+				ParseRoot.ParseOneSentence(online, evalVisitor, false);
+			} catch (Exception e) {
+				// e.printStackTrace();
+				// testing
+				System.err.println("TEST: Wrong parse sentence:" + online);
+			}
 		}
 		br.close();
 		return pp;
