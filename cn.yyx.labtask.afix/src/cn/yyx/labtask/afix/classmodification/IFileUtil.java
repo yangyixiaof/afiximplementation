576f81be25b333a359299870af0b1e92076d0d51
yyx <yyx@ubuntu>
2016-07-05 08:40:42 -0700

    small change.

---
+++ cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/classmodification/IFileUtil.java
@@ -0,0 +1,21 @@
+package cn.yyx.labtask.afix.classmodification;
+
+import java.io.File;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
+public class IFileUtil {
+	
+	public static IFile FileToIFile(File file)
+	{
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IPath location = Path.fromOSString(file.getAbsolutePath());
+		IFile ifile = workspace.getRoot().getFileForLocation(location);
+		return ifile;
+	}
+	
+}
