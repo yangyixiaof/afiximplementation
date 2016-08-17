f9c3c2fa7bac3fa312d94ee290390334f51246fd
yyx <yyx@ubuntu>
2016-08-16 23:24:30 -0700

    small change.

--- cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/controlflow/PrintUtil.java
+++ cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/controlflow/PrintUtil.java
@@ -1,6 +1,7 @@
 package cn.yyx.labtask.afix.controlflow;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
 import com.ibm.wala.examples.drivers.PDFTypeHierarchy;
@@ -16,44 +17,52 @@ import com.ibm.wala.viz.DotUtil;
 import com.ibm.wala.viz.PDFViewUtil;
 
 public class PrintUtil {
-	
+
 	private final static String PDF_FILE = "cg.pdf";
-	
-	public static Process PrintPDF(Graph<CGNode> g) throws WalaException
-	{
+
+	public static Process PrintPDF(Graph<CGNode> g) throws WalaException {
 		Properties p = null;
-	      try {
-	        p = WalaExamplesProperties.loadProperties();
-	        p.putAll(WalaProperties.loadProperties());
-	      } catch (WalaException e) {
-	        e.printStackTrace();
-	        Assertions.UNREACHABLE();
-	      }
-	      String pdfFile = p.getProperty(WalaProperties.OUTPUT_DIR) + File.separatorChar + PDF_FILE;
+		p = WalaExamplesProperties.loadProperties();
+		/*try {
+			p = WalaExamplesProperties.loadProperties();
+			p.putAll(WalaProperties.loadProperties());
+		} catch (WalaException e) {
+			e.printStackTrace();
+			Assertions.UNREACHABLE();
+		}*/
+		String pdfFile = PDF_FILE;// p.getProperty(WalaProperties.OUTPUT_DIR) + File.separatorChar + 
+		File pdff = new File(pdfFile);
+		if (!pdff.exists())
+		{
+			try {
+				pdff.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
-	      String dotExe = p.getProperty(WalaExamplesProperties.DOT_EXE);
-	      DotUtil.dotify(g, null, PDFTypeHierarchy.DOT_FILE, pdfFile, dotExe);
+		String dotExe = p.getProperty(WalaExamplesProperties.DOT_EXE);
+		DotUtil.dotify(g, null, PDFTypeHierarchy.DOT_FILE, pdfFile, dotExe);
 
-	      String gvExe = p.getProperty(WalaExamplesProperties.PDFVIEW_EXE);
-	      return PDFViewUtil.launchPDFView(pdfFile, gvExe);
+		String gvExe = p.getProperty(WalaExamplesProperties.PDFVIEW_EXE);
+		return PDFViewUtil.launchPDFView(pdfFile, gvExe);
 	}
-	
-	public static Process PrintIR(ClassHierarchy cha, IR ir) throws WalaException
-	{
-		 Properties wp = null;
-	      try {
-	        wp = WalaProperties.loadProperties();
-	        wp.putAll(WalaExamplesProperties.loadProperties());
-	      } catch (WalaException e) {
-	        e.printStackTrace();
-	        Assertions.UNREACHABLE();
-	      }
-	      String psFile = wp.getProperty(WalaProperties.OUTPUT_DIR) + File.separatorChar + AFixWalaIR.PDF_FILE;
-	      String dotFile = wp.getProperty(WalaProperties.OUTPUT_DIR) + File.separatorChar + PDFTypeHierarchy.DOT_FILE;
-	      String dotExe = wp.getProperty(WalaExamplesProperties.DOT_EXE);
-	      String gvExe = wp.getProperty(WalaExamplesProperties.PDFVIEW_EXE);
 
-	      return PDFViewUtil.ghostviewIR(cha, ir, psFile, dotFile, dotExe, gvExe);
+	public static Process PrintIR(ClassHierarchy cha, IR ir) throws WalaException {
+		Properties wp = null;
+		try {
+			wp = WalaProperties.loadProperties();
+			wp.putAll(WalaExamplesProperties.loadProperties());
+		} catch (WalaException e) {
+			e.printStackTrace();
+			Assertions.UNREACHABLE();
+		}
+		String psFile = wp.getProperty(WalaProperties.OUTPUT_DIR) + File.separatorChar + AFixWalaIR.PDF_FILE;
+		String dotFile = wp.getProperty(WalaProperties.OUTPUT_DIR) + File.separatorChar + PDFTypeHierarchy.DOT_FILE;
+		String dotExe = wp.getProperty(WalaExamplesProperties.DOT_EXE);
+		String gvExe = wp.getProperty(WalaExamplesProperties.PDFVIEW_EXE);
+
+		return PDFViewUtil.ghostviewIR(cha, ir, psFile, dotFile, dotExe, gvExe);
 	}
-	
+
 }
