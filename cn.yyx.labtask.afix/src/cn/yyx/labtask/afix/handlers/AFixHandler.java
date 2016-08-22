7ed34c55115adc5fce1eb77669835b74881dcc83
yyx <yyx@ubuntu>
2016-08-21 19:02:00 -0700

    small change.

--- cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/handlers/AFixHandler.java
+++ cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/handlers/AFixHandler.java
@@ -12,6 +12,7 @@ import cn.yyx.labtask.afix.ideutil.EclipseHelper;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
+ * 
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */
@@ -21,21 +22,25 @@ public class AFixHandler extends AbstractHandler {
 	 */
 	public AFixHandler() {
 	}
-	
+
 	/**
 	 * the command has been executed, so extract extract the needed information
 	 * from the application context.
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		String demo_Example_content = EclipseHelper.GetContentOfAResource("RaceReport/report_demo_Example");
 		// System.err.println("testcontent:" + testcontent);
-		// System.err.println("==================== split line in legend hahaha ====================");
-		FixHandler.HandleRaceReport(demo_Example_content, "Demo_Example", "demo.Example");
+		// System.err.println("==================== split line in legend hahaha
+		// ====================");
+
+		// FixHandler.HandleRaceReport(EclipseHelper.GetContentOfAResource("RaceReport/report_demo_Authenticate"),
+		// "Demo_Authenticate", "demo.Authenticate");
+		FixHandler.HandleRaceReport(EclipseHelper.GetContentOfAResource("RaceReport/report_demo_Example2"),
+				"Demo_Example2", "demo.Example2");
+		// FixHandler.HandleRaceReport(EclipseHelper.GetContentOfAResource("RaceReport/report_demo_Example"),
+		// "Demo_Example", "demo.Example");
+
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"Afix",
-				"The process has been run over.");
+		MessageDialog.openInformation(window.getShell(), "Afix", "The process has been run over.");
 		return null;
 	}
 }
