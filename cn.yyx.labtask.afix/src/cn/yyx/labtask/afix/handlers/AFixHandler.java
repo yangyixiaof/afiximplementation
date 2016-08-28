5550740afbc26a849698904b24ee36453ade93b7
yyx <yyx@ubuntu>
2016-08-27 21:26:01 -0700

    small change.

--- cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/handlers/AFixHandler.java
+++ cn.yyx.labtask.afix/src/cn/yyx/labtask/afix/handlers/AFixHandler.java
@@ -9,7 +9,11 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TreeNode;
+import org.eclipse.jface.viewers.TreeNodeContentProvider;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.dialogs.ElementTreeSelectionDialog;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import cn.yyx.labtask.afix.FixHandler;
@@ -36,12 +40,25 @@ public class AFixHandler extends AbstractHandler {
 		// System.err.println("testcontent:" + testcontent);
 		// System.err.println("==================== split line in legend hahaha
 		// ====================");
+		final IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		
 		ProgressMonitorDialog dialog = new ProgressMonitorDialog(HandlerUtil.getActiveShell(event));
 		try {
 			dialog.run(true, true, new IRunnableWithProgress() {
 				@Override
 				public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+					
+					ElementTreeSelectionDialog  d=new  ElementTreeSelectionDialog(window.getShell(), new LabelProvider(), new TreeNodeContentProvider());
+					TreeNode  input=new TreeNode("root");
+					TreeNode  node1=new TreeNode("node1");
+					TreeNode  node2=new TreeNode("node2");
+					 
+					input.setChildren(new TreeNode[]{node1,node2});
+					d.setInput(new TreeNode[]{input}); 
+					d.open();
+					
+					
+					
 					monitor.beginTask("Start Task", 100);
 					// example code.
 					/*for (int i = 0; i < 100; i++) {
@@ -59,8 +76,8 @@ public class AFixHandler extends AbstractHandler {
 					//		"Demo_Example2", "demo.Example2", monitor);
 					// FixHandler.HandleRaceReport(EclipseHelper.GetContentOfAResource("RaceReport/report_demo_Example"),
 					//		"Demo_Example", "demo.Example", monitor);
-					FixHandler.HandleRaceReport(EclipseHelper.GetContentOfAResource("RaceReport/report_account_Account"),
-							"Account_Account", "account.Account", monitor);
+					// FixHandler.HandleRaceReport(EclipseHelper.GetContentOfAResource("RaceReport/report_account_Account"),
+					//		"Account_Account", "account.Account", monitor);
 					// FixHandler.HandleRaceReport(EclipseHelper.GetContentOfAResource("RaceReport/report_critical_Critical"),
 					//		"Critical_Critical", "critical.Critical", monitor);
 					// FixHandler.HandleRaceReport(EclipseHelper.GetContentOfAResource("RaceReport/report_pingpong_PingPong"),
@@ -74,7 +91,6 @@ public class AFixHandler extends AbstractHandler {
 			e.printStackTrace();
 		}
 		
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		MessageDialog.openInformation(window.getShell(), "Afix", "The process has been run over.");
 		return null;
 	}
